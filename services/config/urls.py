1c6e66717e839ae2c857f5050ddd6368cc4741de
Daniel Caraballo <danielcaraballoponte@gmail.com>
2025-05-10 13:07:09 -0400

    feat(admin): cambiar ruta base del admin de django

--- services/config/urls.py
+++ services/config/urls.py
@@ -32,9 +32,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'), 
+    path('', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/productos/', include('apps.productos.urls')),
